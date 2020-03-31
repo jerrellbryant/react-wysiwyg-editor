db92b9ca7b856b8fcb02b49005e9d07a65c31dc1
jerrellbrant <jerrellkbryant@icloud.com>
2020-03-31 15:42:02 -0400

    getText button added to Buttons component and changed class name of div in Editor component and updated styles for displayContent class

--- src/Buttons.js
+++ src/Buttons.js
@@ -3,15 +3,23 @@ import { connect } from "react-redux";
 
 function Buttons({editorRef}) {
     const [showContent, setShowContent] = useState(false);
-    const [content, setContent] = useState({__html: ""});
+    const [content, setContent] = useState({});
 
     const getHtml = () => {
         if(!editorRef.current) return; // bail early if no ref available
-        const html = editorRef.current.innerHTML;
-
+         const html = editorRef.current.innerHTML;
         //Note: check if you have to sanitize the html
-        setContent({__html: html});
+        setContent({ __html : html });
         setShowContent(true);
+    };
+
+    const getText = () => {
+        if(!editorRef.current) return; // bail early if no ref available
+        const text = editorRef.current.textContent;
+        //Note: check if you have a sanitize the text
+        setContent({ __text: text });
+        setShowContent(true);
+
     };
 
     const print = () => {
@@ -27,16 +35,19 @@ function Buttons({editorRef}) {
         <>
             <div className="center">
                 <button className="btn" onClick={getHtml}>GetHtml</button>
+                <button className="btn" onClick={getText}>GetText</button>
                 <button className="btn" onClick={print}>Print</button>
             </div>
 
-            <div className="center">
+            <div className="center section-to-print">
                 <section
-                    className="getcontent"
-                    style={{display: showContent ? "block" : "none"}}
-                    dangerouslySetInnerHTML={content}
-                />
-                <code>{content.__html}</code>
+                    className="displayContent"
+                    style={{display: showContent ? "block" : "none"}}>
+                    <span>
+                        {content.__html || content.__text}
+                    </span>
+                </section>
+
             </div>
 
         </>
--- src/Editor.js
+++ src/Editor.js
@@ -10,26 +10,26 @@ function Editor(props) {
     const [introText] = useState("Simple React Editor");
     const [subText] = useState("Good to start");
     const [images, setImages] = useState([]);
+    //
+    // const getStyles = () => {
+    //     const BLACK = "#000000";
+    //     const VW = "vw";
+    //     const VH = "vh";
+    //     const REM = "rem";
+    //
+    //     return {
+    //         width: `70${VW}`,
+    //         height: `50${VH}`,
+    //         margin: `2${REM}`,
+    //         padding: `1${REM}`,
+    //         fontSize: `1.2${REM}`,
+    //         boxShadow: `0 .1${REM} .4rem ${BLACK}`,
+    //         border: `1px solid ${BLACK}`,
+    //         overflowY: `auto`
+    //     };
+    // };
 
-    const getStyles = () => {
-        const BLACK = "#000000";
-        const VW = "vw";
-        const VH = "vh";
-        const REM = "rem";
-
-        return {
-            width: `70${VW}`,
-            height: `50${VH}`,
-            margin: `2${REM}`,
-            padding: `1${REM}`,
-            fontSize: `1.2${REM}`,
-            boxShadow: `0 .1${REM} .4rem ${BLACK}`,
-            border: `1px solid ${BLACK}`,
-            overflowY: `auto`
-        };
-    };
-
-    const editorStyle = getStyles();
+    // const editorStyle = getStyles();
 
         return (
             <>
@@ -38,7 +38,7 @@ function Editor(props) {
                 <div className="center section-to-print">
                     <div
                         ref={editorRef}
-                        style={editorStyle}
+                        className="editor"
                         contentEditable={true}
                         suppressContentEditableWarning={true}
                     >
